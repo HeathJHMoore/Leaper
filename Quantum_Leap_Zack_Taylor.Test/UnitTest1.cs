513143583aec43bcff60845ac3163636e897ba15
Heath Moore <heathjhmoore@gmail.com>
2019-09-17 19:31:46 -0500

    Add project files.

---
+++ Quantum_Leap_Zack_Taylor.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Quantum_Leap_Zack_Taylor.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Quantum_Leap_Zack_Taylor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quantum_Leap_Zack_Taylor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
