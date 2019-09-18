583fe80fe16ca4a9f76bd8dae8c6fb8424b5974d
Heath Moore <heathjhmoore@gmail.com>
2019-09-17 20:39:37 -0500

    created budget class

---
+++ Quantum_Leap_Zack_Taylor/LeapComponents/Budget.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quantum_Leap_Zack_Taylor.LeapComponents
+{
+    class Budget
+    {
+        private int _budget { get; set; }
+
+        public Budget()
+        {
+            _budget = 3000;
+        }
+
+        public bool CheckBudget()
+        {
+            if (_budget < 1000)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void AddFunds()
+        {
+            _budget += 5000;
+        }
+    }
+}
