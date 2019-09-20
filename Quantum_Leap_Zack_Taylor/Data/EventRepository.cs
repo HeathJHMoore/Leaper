ccad947fca0d5df0d1c51247170649a8c28efe3b
Bob <bob.e.baxter@gmail.com>
2019-09-19 22:47:17 -0500

    eventRepository class works

---
+++ Quantum_Leap_Zack_Taylor/Data/EventRepository.cs
@@ -0,0 +1,44 @@
+﻿using Quantum_Leap_Zack_Taylor.LeapComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quantum_Leap_Zack_Taylor.Data
+{
+    class EventRepository
+    {
+        static List<Event> _listOfEvents = new List<Event>();
+
+        public List<Event> GetAll()
+        {
+            return _listOfEvents;
+        }
+        
+        public void Add(Event eventToAdd)
+        {
+            eventToAdd.Id = Guid.NewGuid();
+            eventToAdd.Date = new DateTime();
+            eventToAdd.IsPutRight = false;
+            _listOfEvents.Add(eventToAdd);
+        }
+
+        public void Remove(Event eventToRemove)
+        {
+            _listOfEvents.Remove(eventToRemove);
+        }
+
+        public void PopulateEvents()
+        {
+            // add event seed data here with the above add method
+            // then call this method at the top of Program.cs
+        }
+
+        public Event GetRandomEvent()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(_listOfEvents.Count);
+            var randomEvent = _listOfEvents[index];
+            return randomEvent;
+        }
+    }
+}
